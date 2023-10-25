58854fa5ac0f0904458a382be25f70c402beb063
adrienne_b <WorldofArts_95@yahoo.com>
2023-10-25 07:16:48 -0500

    Added picture png again inside of folder..testing, added padded to the left and top, temporary replacement of the first bike.

--- src/components/ReservationItem.js
+++ src/components/ReservationItem.js
@@ -7,16 +7,16 @@ import React from 'react'
 
 function ReservationItem({images, name,  description, price}) {
   return (
-    <div className='reservationItem'>
+    <div style={{paddingLeft: "11rem", paddingTop: "7rem"}} className='reservationItem'>
          {/* <div style={{ backgroundImage: `url(${images})` }}> </div> */}
         {/* <div><img src={Blueberry} alt="berry"/></div>
         <div><img src={Wendy} alt="wendy"/></div>
         <div><img src={Bee} alt="bee"/></div>
         <div><img src={Sunset} alt="bee"/></div> */}
-        <div><img src={images} alt="image" /></div>
+        <div><img style={{boxShadow: "2px 2px #999"}} src={images} alt="image" /></div>
         {/* <div>{images}</div> */}
         <h6 style={{fontSize: "18px"}}>{name}</h6>
-        <h5 style={{color: "#7A7C83"}}>{description}</h5>
+        <h5 style={{color: "#7A7C83", wordBreak: "break-all",  width: "18rem"}}>{description}</h5>
         <p style={{fontWeight: "bold"}}>${price}</p>
     </div> 
   )
--- src/helpers/ReservationList.js
+++ src/helpers/ReservationList.js
@@ -1,11 +1,11 @@
-import Blueberry from "../images/Blueberry.png";
+// import BlueSky from "../images/BlueSky.png";
 import Wendy from "../images/wendy_wednesday.png"
 import Bee from "../images/bee_stinger.png";
 import Sunset from "../images/sunset_blackout.png";
 
 export const ReservationList = [
     {
-        image: Blueberry,
+        images: Wendy,
         name: "Blueberry Benz",
         description: `If you want to make reservations for a special occassion hit the button below`,
         price: 34.45,
