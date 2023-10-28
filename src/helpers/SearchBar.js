6d3bb6e21e3d12f78e91d13872f573bf1e256959
adrienne_b <WorldofArts_95@yahoo.com>
2023-10-28 07:53:37 -0500

    I had to give the mui icon magnify glass its own div for it to appear in the browser and then I added style in the searchBar.css.

--- src/helpers/SearchBar.js
+++ src/helpers/SearchBar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import '../styles/SearchBar.css';
-import {AiOutlineSearch} from 'react-icons/ai'
+import SearchIcon from '@mui/icons-material/Search';
 
+console.log(SearchIcon );
 
 function SearchBar() {
   return (
     <div className= "SearchBar">
         <form class="example" action="action_page.php">
           <input type="text" placeholder="Search.."></input>
-          <button id="buttonHome" type="submit"><AiOutlineSearch/></button>     
+          <button id="buttonHome" type="submit"></button>
+          <div style={{color: "white"}} className='svg'><SearchIcon/></div>    
        </form>
     </div>
   )
 }
 
 export default SearchBar
+
+//         <button id="buttonHome" type="submit" style={{color: "blue"}}><SearchIcon/></button>
